f723d9440b0b9093b48fa7c56c953b39fef9ada6
Jordan Dearsley <jordan@dearsley.dev>
2023-09-29 13:21:40 -0700

    Checking in

--- vapi.ts
+++ vapi.ts
@@ -4,27 +4,59 @@ import {
   IAudioBufferSourceNode,
   IAudioContext,
 } from "standardized-audio-context";
+import async, { QueueObject } from "async";
 
 import { client } from "./client";
 import { decode } from "base64-arraybuffer";
 
 export default class Vapi {
-  private audioContext: AudioContext | null = null;
+  private audioContext: AudioContext = new AudioContext();
   private source: IAudioBufferSourceNode<IAudioContext> | null = null;
   private started: boolean = false;
   private ws: WebSocket | null = null;
   private mediaRecorder: MediaRecorder | null = null;
 
+  private nextClipTime: number = 0;
+
+  private decodeQueue: QueueObject<ArrayBuffer>;
+  private playQueue: QueueObject<AudioBuffer>;
+
   constructor(apiToken: string) {
     client.setSecurityData(apiToken);
+
+    this.decodeQueue = async.queue((task, callback) => {
+      this.audioContext.decodeAudioData(task, (buffer) => {
+        this.playQueue.push(buffer);
+        callback();
+      });
+    }, 1);
+
+    this.playQueue = async.queue((buffer, callback) => {
+      this.source = this.audioContext.createBufferSource();
+      this.source.buffer = buffer;
+      this.source.connect(this.audioContext.destination);
+
+      // If the next clip time is in the past, set it to the current time
+      this.nextClipTime = Math.max(
+        this.nextClipTime,
+        this.audioContext.currentTime
+      );
+
+      // Start the source at the next clip time
+      this.source.start(this.nextClipTime);
+
+      // Schedule the next clip time
+      this.nextClipTime += buffer.duration;
+
+      // When this source ends, call the callback to process the next audio clip
+      this.source.onended = (e) => callback();
+    }, 1);
   }
 
   start(agent: CreateAgentDTO): void {
     if (this.started) {
       return;
     }
-
-    this.audioContext = new AudioContext();
 
     this.started = true;
 
@@ -74,16 +106,7 @@ export default class Vapi {
     const data = JSON.parse(event.data);
     if (data.event === "media") {
       const audioData = decode(data.media.payload);
-      if (!this.audioContext) return;
-
-      this.audioContext.decodeAudioData(audioData, (buffer) => {
-        if (!this.audioContext) return;
-
-        this.source = this.audioContext.createBufferSource();
-        this.source.buffer = buffer;
-        this.source.connect(this.audioContext.destination);
-        this.source.start(0);
-      });
+      this.decodeQueue.push(audioData);
     }
   }
 
@@ -100,10 +123,5 @@ export default class Vapi {
       this.mediaRecorder.stop();
     }
     this.mediaRecorder = null;
-
-    if (this.audioContext) {
-      this.audioContext.close();
-    }
-    this.audioContext = null;
   }
 }
