b312a97e315fdb8b27ea9145c025b822bb545af4
Jordan Dearsley <jordan@dearsley.dev>
2023-10-04 23:28:27 -0700

    Checking in

---
+++ AtomicMediaSource.ts
@@ -0,0 +1,58 @@
+import async, { QueueObject } from "async";
+
+class AtomicMediaSource {
+  mediaSource: MediaSource;
+  private sourceBuffer: SourceBuffer | null = null;
+  private operationsQueue: QueueObject<() => Promise<void>>;
+
+  constructor() {
+    this.mediaSource = new MediaSource();
+    this.operationsQueue = this.createOperationQueue();
+
+    this.mediaSource.addEventListener("sourceopen", () => {
+      this.mediaSource.duration = Number.POSITIVE_INFINITY;
+      this.sourceBuffer = this.mediaSource.addSourceBuffer(
+        'audio/webm; codecs="opus"'
+      );
+      this.sourceBuffer.mode = "sequence";
+    });
+  }
+
+  public appendBuffer(buffer: ArrayBuffer) {
+    this.operationsQueue.push(() => {
+      return new Promise((resolve, reject) => {
+        if (this.sourceBuffer) {
+          this.sourceBuffer.addEventListener("updateend", () => {
+            resolve();
+          });
+          this.sourceBuffer.appendBuffer(buffer);
+        } else {
+          reject();
+        }
+      });
+    });
+  }
+
+  public clearBuffer() {
+    this.operationsQueue.push(() => {
+      return new Promise((resolve, reject) => {
+        if (this.sourceBuffer) {
+          this.sourceBuffer.addEventListener("updateend", () => {
+            resolve();
+          });
+          this.sourceBuffer.remove(0, this.mediaSource.duration);
+        } else {
+          reject();
+        }
+      });
+    });
+  }
+
+  private createOperationQueue() {
+    return async.queue<() => Promise<void>>((task, callback) => {
+      task().then(() => callback());
+    }, 1);
+  }
+}
+
+export default AtomicMediaSource;
--- player.ts
+++ player.ts
@@ -1,42 +1,22 @@
-import async, { QueueObject } from "async";
-
+import AtomicMediaSource from "./AtomicMediaSource";
 import EventEmitter from "events";
 
 export class ContinuousPlayer extends EventEmitter {
-  private mediaSource: MediaSource;
-  private sourceBuffer: SourceBuffer | null = null;
+  private atomic: AtomicMediaSource;
   private audio: HTMLAudioElement;
-  private operationsQueue: QueueObject<() => Promise<void>>;
 
   constructor() {
     super();
-    this.mediaSource = new MediaSource();
+    this.atomic = new AtomicMediaSource();
 
     this.audio = document.createElement("audio");
 
     this.audio.addEventListener("playing", () => this.emit("speech-start"));
     this.audio.addEventListener("play", () => this.emit("speech-start"));
-
     this.audio.addEventListener("waiting", () => this.emit("speech-end"));
 
-    this.audio.src = URL.createObjectURL(this.mediaSource);
+    this.audio.src = URL.createObjectURL(this.atomic.mediaSource);
     document.body.appendChild(this.audio);
-
-    this.operationsQueue = this.createOperationQueue();
-
-    this.mediaSource.addEventListener("sourceopen", () => {
-      this.mediaSource.duration = Number.POSITIVE_INFINITY;
-      this.sourceBuffer = this.mediaSource.addSourceBuffer(
-        'audio/webm; codecs="opus"'
-      );
-      this.sourceBuffer.mode = "sequence";
-    });
-  }
-
-  createOperationQueue() {
-    return async.queue<() => Promise<void>>((task, callback) => {
-      task().then(() => callback());
-    }, 1);
   }
 
   start() {
@@ -44,71 +24,10 @@ export class ContinuousPlayer extends EventEmitter {
   }
 
   playChunk(audioData: ArrayBuffer) {
-    console.log("playChunk");
-    this.operationsQueue.push(() => this.appendNextChunk(audioData));
-  }
-
-  private performBufferOperation(operation: () => void) {
-    return new Promise<void>((resolve) => {
-      if (!this.sourceBuffer) {
-        resolve();
-        return;
-      }
-
-      if (this.sourceBuffer.updating) {
-        this.sourceBuffer.addEventListener(
-          "updateend",
-          () => {
-            operation();
-            resolve();
-          },
-          {
-            once: true,
-          }
-        );
-        return;
-      }
-
-      operation();
-      resolve();
-    });
-  }
-
-  private appendNextChunk(chunk: ArrayBuffer) {
-    return this.performBufferOperation(() => {
-      if (!this.sourceBuffer) return;
-      console.log("appending");
-
-      this.sourceBuffer.appendBuffer(chunk);
-    });
-  }
-
-  private removeBuffer() {
-    return this.performBufferOperation(() => {
-      if (!this.sourceBuffer) return;
-
-      if (this.sourceBuffer.buffered.length > 0) {
-        this.sourceBuffer.remove(
-          0,
-          this.sourceBuffer.buffered.end(this.sourceBuffer.buffered.length - 1)
-        );
-      }
-    });
-  }
-
-  private resetTimestampOffset() {
-    return this.performBufferOperation(() => {
-      if (!this.sourceBuffer) return;
-      this.sourceBuffer.timestampOffset = 0;
-    });
+    this.atomic.appendBuffer(audioData);
   }
 
   clear() {
-    // if (!this.sourceBuffer) return;
-    // this.sourceBuffer.abort();
-    // this.operationsQueue.kill();
-    // this.operationsQueue = this.createOperationQueue();
-    // this.operationsQueue.push(() => this.removeBuffer());
-    // this.operationsQueue.push(() => this.resetTimestampOffset());
+    this.atomic.clearBuffer();
   }
 }
