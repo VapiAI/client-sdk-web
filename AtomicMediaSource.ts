65c5bc566004c6fbba85f6d37c69377a5bf2b9f1
Jordan Dearsley <jordan@dearsley.dev>
2023-10-04 23:59:12 -0700

    Checking in

--- AtomicMediaSource.ts
+++ AtomicMediaSource.ts
@@ -40,23 +40,46 @@ class AtomicMediaSource {
 
   public resetBuffer() {
     this.operationsQueue.push(async () => {
-      if (this.sourceBuffer) {
-        await new Promise<void>((resolve) => {
-          this.mediaSource.removeSourceBuffer(this.sourceBuffer!);
-          this.sourceBuffer!.addEventListener(
-            "updateend",
-            () => {
-              console.log("Source buffer removed");
+      try {
+        // Check if mediaSource exists
+        if (!this.mediaSource) {
+          console.error("mediaSource is not available.");
+          return;
+        }
+
+        // Remove existing source buffer if it exists
+        if (this.sourceBuffer) {
+          const removalPromise = new Promise<void>((resolve, reject) => {
+            const onRemoval = () => {
+              this.mediaSource.removeEventListener("updateend", onRemoval);
               resolve();
-            },
-            { once: true }
+            };
+
+            this.mediaSource.addEventListener("updateend", onRemoval, {
+              once: true,
+            });
+            try {
+              this.mediaSource.removeSourceBuffer(this.sourceBuffer!);
+            } catch (error) {
+              reject(error);
+            }
+          });
+
+          await removalPromise;
+        }
+
+        // Add a new source buffer
+        try {
+          this.sourceBuffer = this.mediaSource.addSourceBuffer(
+            'audio/webm; codecs="opus"'
           );
-        });
+          this.sourceBuffer.mode = "sequence";
+        } catch (error) {
+          console.error("Error adding source buffer:", error);
+        }
+      } catch (error) {
+        console.error("Error in resetBuffer:", error);
       }
-      this.sourceBuffer = this.mediaSource.addSourceBuffer(
-        'audio/webm; codecs="opus"'
-      );
-      this.sourceBuffer.mode = "sequence";
     });
   }
 
