b8346d87fd05460ee01a11fefb0187efe707f32d
Jordan Dearsley <jordan@dearsley.dev>
2023-10-14 16:06:29 -0700

    Checking in

--- vapi.ts
+++ vapi.ts
@@ -44,7 +44,6 @@ export default class Vapi extends EventEmitter {
           (e) => e && this.handleSpeakerChange(e)
         );
 
-        console.log("adddding");
         this.call.startRemoteParticipantsAudioLevelObserver();
         this.call.on("remote-participants-audio-level", (e) => {
           console.log(e);
