45424c9835798bed5fbb9c5b02f5115816b8fe58
Mhar Andal <me@mharvelous.io>
2026-06-03 15:56:56 -0700

    chore: update dependencies

--- example/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_VAPI_PUBLIC_KEY: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-} 
--- update.sh
+++
@@ -1,1 +0,0 @@
-npm version patch && npm run build && npm publish && cd ../landing && npm install @vapi-ai/web@latest && cd -
